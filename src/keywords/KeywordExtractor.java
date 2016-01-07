01934f0869e75ccc00c03bcee66db1d1724a9012
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-01-07 15:34:34 +0100

    Renamed class and removed useless statements

---
+++ src/keywords/KeywordExtractor.java
@@ -0,0 +1,20 @@
+package keywords;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import com.alchemyapi.api.AlchemyAPI;
+
+public class KeywordExtractor {
+
+	public static Document extractKeyword(String text, String api_path) throws IOException, XPathExpressionException, SAXException, ParserConfigurationException {
+		AlchemyAPI alchemyObj = AlchemyAPI.GetInstanceFromFile(api_path);
+        return alchemyObj.TextGetRankedKeywords(text);
+	}
+
+}
