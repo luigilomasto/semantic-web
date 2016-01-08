d612dcea5de3f89dcfe596bb47cdc00dd66d6c5d
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-01-08 18:35:59 +0100

    Fixed print function for keywords

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Web Semantic Project
+
+Put api_key.txt in lib folder in order to work.
--- src/keywords/scraperForKeyword.java
+++ src/keywords/scraperForKeyword.java
@@ -4,8 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.w3c.dom.Document;
@@ -17,12 +23,29 @@ public class scraperForKeyword {
 	private static BufferedReader reader;
 	private static int numeroRigheLette;
 	private static int nArticoli;
+	
+	private static String getStringFromDocument(Document doc) {
+        try {
+            DOMSource domSource = new DOMSource(doc);
+            StringWriter writer = new StringWriter();
+            StreamResult result = new StreamResult(writer);
+
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.transform(domSource, result);
+
+           return writer.toString();
+        } catch (TransformerException ex) {
+            ex.printStackTrace();
+            return null;
+    }
+    }
 
 	public static void main(String [] args) throws IOException, XPathExpressionException, SAXException, ParserConfigurationException{
 		numeroRigheLette=0;
 		nArticoli = 0;
 		boolean abstractB=false;
-		reader = new BufferedReader(new FileReader("/home/luigi/git/solid-memory/lib/datasetWithAbstract/dataset1.xml"));
+		reader = new BufferedReader(new FileReader("lib/datasetWithAbstract/dataset1.xml"));
 
 
 		String line = reader.readLine();
@@ -30,7 +53,7 @@ public class scraperForKeyword {
 
 
 		FileWriter w;
-		w=new FileWriter("/home/luigi/git/solid-memory/lib/datasetWithKeyWord/dataset1.xml");
+		w=new FileWriter("lib/datasetWithKeyWord/dataset1.xml");
 
 
 		while(line!=null) {
@@ -53,9 +76,9 @@ public class scraperForKeyword {
 					abstract_Text=abstract_Text.replace("</abstract>","");
 					if(!abstract_Text.equals("")){
 						//System.out.println(abstract_Text);
-						//strano o.o a me funzionava, provo a debuggare
-						Document s=KeywordExtractor.extractKeyword(abstract_Text, "/home/luigi/git/solid-memory/lib/API_key/marco");
-						System.out.println(s);
+						Document s=KeywordExtractor.extractKeyword(abstract_Text, "lib/api_key.txt");
+						String keyworddocument = getStringFromDocument(s);
+						System.out.println(keyworddocument);
 					}
 
 					abstract_Text="";
@@ -71,6 +94,7 @@ public class scraperForKeyword {
 
 			line = reader.readLine();
 		}
+		w.close();
 		System.out.println(nArticoli);
 
 	}
